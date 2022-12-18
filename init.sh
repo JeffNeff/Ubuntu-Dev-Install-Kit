84b0efde08741719d7cb835f39faf64655c9cdfb
Jeff <60796676+JeffNeff@users.noreply.github.com>
2022-12-18 13:43:58 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,8 +11,8 @@ echo 'export PATH=$PATH:/home/$USER/go/bin' >> ~/.profile
 ## Install Node.
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 source ~/.profile
-nvm install 16
-nvm use 16
+nvm install 18
+nvm use 18
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 node i yarn --global
