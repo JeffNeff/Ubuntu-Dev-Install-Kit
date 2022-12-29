79b1245f2aab0414d6ab7dce57eb657b3663d0f4
Jeff <60796676+JeffNeff@users.noreply.github.com>
2022-12-28 17:46:41 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ nvm install 18
 nvm use 18
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-node i yarn --global
+ npm i yarn --global
 
 ## Install Docker.
 curl -fsSL https://get.docker.com -o get-docker.sh
